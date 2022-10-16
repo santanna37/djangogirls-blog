e0288e723849ffd0f123248e977ea988f9c63c39
santanna37 <santanna.ilha@gmail.com>
2022-10-15 22:38:45 -0300

    c6 - querys arrumadas e rodando- começar os templates

--- blog/views.py
+++ blog/views.py
@@ -6,10 +6,8 @@ from django.utils import timezone
 # Create your views here.
 #camando o html da inicial 
 def post_list(request):
-    posts =Post.objects.filter(
-        published_dete__lte=timezone.now()).order_by('published_date')
-    
-    return render(request, 'blog/post_list.html',{'post':posts})
+    posts = Post.objects.order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts':post})
 
 # =================
 # power query para ordenar os comentários 
